18a9062be348af03e894c6c2d077ac2e829dcfbb
kelekelio <cristal01@gmail.com>
2020-10-21 17:47:53 +0200

    register

---
+++ src/main/java/com/example/wypozyczalnia/user/UserController.java
@@ -0,0 +1,23 @@
+package com.example.wypozyczalnia.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.request.WebRequest;
+
+/**
+ * @author Grzegorz Nowakowski
+ */
+@Controller
+public class UserController {
+
+    @GetMapping("/user/registration")
+    public String showRegistrationForm(WebRequest request, Model model) {
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
+        return "registration";
+    }
+
+}
