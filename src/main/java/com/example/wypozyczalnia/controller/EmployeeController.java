bb5fea331aac98893805d9b4389fd1bcac4d4fe6
ktynor <kt9@tlen.pl>
2020-10-22 21:22:24 +0100

    mapping for update and delete employee done but doesn't work yet. An error in updateEmployee.html or EmployeeController

--- src/main/java/com/example/wypozyczalnia/controller/EmployeeController.java
+++ src/main/java/com/example/wypozyczalnia/controller/EmployeeController.java
@@ -1,13 +1,13 @@
 package com.example.wypozyczalnia.controller;
 
 import com.example.wypozyczalnia.model.Employee;
+import com.example.wypozyczalnia.repository.EmployeeRepository;
 import com.example.wypozyczalnia.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
 
@@ -15,6 +15,8 @@ import java.util.List;
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
+    @Autowired
+    EmployeeRepository employeeRepository;
     @RequestMapping("/employeeList")
     public String viewEmployeeList(Model model) {
         List<Employee> employeeList = employeeService.listAll();
@@ -30,6 +32,17 @@ public class EmployeeController {
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public String saveEmployee(@ModelAttribute("employee") Employee employee) {
         employeeService.save(employee);
+        return "redirect:/employeeList";
+    }
+    @GetMapping("showUpdateEmployeeForm/{id}")
+    public RedirectView showUpdateEmployeeForm(@PathVariable(value="id") long id,Model model) {
+        Employee employee = employeeService.get(id);
+        model.addAttribute("employee", employee);
+        return new RedirectView("employee/updateEmployee");
+    }
+    @GetMapping({"employee/employeeList/{id}"})
+    public String deleteEmployee(Model model, @PathVariable("id") Long id) {
+        employeeService.delete(id);
         return "redirect:/employeeList";
     }
 }
