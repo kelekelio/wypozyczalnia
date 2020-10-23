4e39d532f5ccf7438d07dcc69423738f07ad06c2
ktynor <kt9@tlen.pl>
2020-10-23 18:31:06 +0100

    zagmatwalem sie w updateEmployee.html, ale zapisze na razie zmiany - dalej nie dziala mapping, kod 400 lub 500

--- src/main/java/com/example/wypozyczalnia/controller/EmployeeController.java
+++ src/main/java/com/example/wypozyczalnia/controller/EmployeeController.java
@@ -17,30 +17,58 @@ public class EmployeeController {
     private EmployeeService employeeService;
     @Autowired
     EmployeeRepository employeeRepository;
+
     @RequestMapping("/employeeList")
     public String viewEmployeeList(Model model) {
         List<Employee> employeeList = employeeService.listAll();
         model.addAttribute("employeeList", employeeList);
         return "employee/employeeList";
     }
+
     @RequestMapping("employee/addNewEmployee")
     public String addNewEmployeeForm(Model model) {
         Employee employee = new Employee();
         model.addAttribute("employee", employee);
         return "employee/addNewEmployee";
     }
+    @RequestMapping("employee/updateEmployee2")
+    public String updateEmployeeForm2(Model model) {
+        Employee employee2 = new Employee();
+        model.addAttribute("employee2", employee2);
+        return "employee/updateEmployee2";
+    }
+    @RequestMapping("employee/updateEmployee")
+    public String updateEmployeeForm(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employee/updateEmployee";
+    }
+
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public String saveEmployee(@ModelAttribute("employee") Employee employee) {
         employeeService.save(employee);
         return "redirect:/employeeList";
     }
-    @GetMapping("showUpdateEmployeeForm/{id}")
-    public RedirectView showUpdateEmployeeForm(@PathVariable(value="id") long id,Model model) {
+    @RequestMapping(value = "/save2", method = RequestMethod.POST)
+    public String saveEmployee2(@ModelAttribute("employee") Employee employee) {
+        employeeService.save(employee);
+        return "redirect:/employeeList";
+    }
+
+    @RequestMapping(value = "/saveUpdate", method = RequestMethod.POST)
+    public String saveUpdateEmployee(@ModelAttribute("employee") Employee employee) {
+        employeeService.save(employee);
+        return "redirect:/employeeList";
+    }
+
+    @RequestMapping({"/showUpdateEmployeeForm/{id}"})
+    public RedirectView showUpdateEmployeeForm(@PathVariable(value = "id") long id, Model model) {
         Employee employee = employeeService.get(id);
         model.addAttribute("employee", employee);
-        return new RedirectView("employee/updateEmployee");
+        return new RedirectView("updateEmployee");
     }
-    @GetMapping({"employee/employeeList/{id}"})
+
+    @RequestMapping({"/employee/employeeList/{id}"})
     public String deleteEmployee(Model model, @PathVariable("id") Long id) {
         employeeService.delete(id);
         return "redirect:/employeeList";
--- src/main/java/com/example/wypozyczalnia/service/EmployeeService.java
+++ src/main/java/com/example/wypozyczalnia/service/EmployeeService.java
@@ -19,6 +19,9 @@ public class EmployeeService {
     public void save(Employee employee) {
         employeeRepository.save(employee);
     }
+    public void saveUpdateEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
 
     public Employee get(Long id) {
 
